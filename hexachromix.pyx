5a72a89693c490839f649bc84dbf16b6e3adc116
Taylor Vance <tmax8908@yahoo.com>
2023-08-11 15:58:25 -0500

    Found the bottleneck--TRANSFORMATIONS 6,13 shape--and reverted to 6,4,2 shape

--- hexachromix.pyx
+++ hexachromix.pyx
@@ -9,40 +9,36 @@ cimport cython
 # Set up some constants.
 COLORS = "RYGCBM"
 
-cdef char[6][13] TRANSFORMATIONS = [
-    #0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12
-    #-, R, Y, G, C, B, M, r, y, g, c, b, m
-    [1,-1,-1, 8,-1,12,-1, 1,-1,-1,-1,-1,-1],
-    [2,-1,-1,-1, 9,-1, 7,-1, 2,-1,-1,-1,-1],
-    [3, 8,-1,-1,-1,10,-1,-1,-1, 3,-1,-1,-1],
-    [4,-1, 9,-1,-1,-1,11,-1,-1,-1, 4,-1,-1],
-    [5,12,-1,10,-1,-1,-1,-1,-1,-1,-1, 5,-1],
-    [6,-1, 7,-1,11,-1,-1,-1,-1,-1,-1,-1, 6],
-]
-
 INT2CHAR = ['-','R','Y','G','C','B','M','r','y','g','c','b','m']
 CHAR2INT = {c:i for i,c in enumerate(INT2CHAR)}
+
+cdef char[6][4][2] TRANSFORMATIONS = [
+    [[CHAR2INT[c] for c in pair] for pair in group] for group in [
+        [['-','R'], ['r','R'], ['G','y'], ['B','m']],
+        [['-','Y'], ['y','Y'], ['C','g'], ['M','r']],
+        [['-','G'], ['g','G'], ['B','c'], ['R','y']],
+        [['-','C'], ['c','C'], ['M','b'], ['Y','g']],
+        [['-','B'], ['b','B'], ['R','m'], ['G','c']],
+        [['-','M'], ['m','M'], ['Y','r'], ['C','b']],
+    ]
+]
 
 
 cdef class HexachromixState:
     cdef char[19] board
-    cdef char _player
+    cdef char player
     cdef variant
 
-    def __init__(self, board:list=[0]*19, player:int=0, variant:str='MRY', hfen:str=None):
+    def __init__(self, board:list=[0]*19, player:int=0, variant:str="MRY", hfen:str=None):
         if hfen is not None:
             (boardstr, color, variant) = hfen.split(' ')
             # Replace ints with dashes, strip slashes, and reverse.
-            boardstr = re.sub(
-                r'\d',
-                lambda x: '-'*int(x.group(0)),
-                boardstr.replace('/','')
-            )
+            boardstr = re.sub(r'\d', lambda x: '-'*int(x.group(0)), boardstr.replace('/',''))
             board = [CHAR2INT[c] for c in boardstr]
             player = COLORS.index(color)
 
         for i in range(19): self.board[i] = board[i]
-        self._player = player
+        self.player = player
         self.variant = variant
 
     @property
@@ -50,40 +46,39 @@ cdef class HexachromixState:
         board = [INT2CHAR[self.board[i]] for i in range(19)]
         board = '/'.join([''.join(board[i:j]) for i,j in [(0,3), (3,7), (7,12), (12,16), (16,19)]])
         board = re.sub(r'-+', lambda x: str(len(x.group(0))), board)
-        return f'{board} {self.player} {self.variant}'
-
-    @property
-    def player(self) -> str: return COLORS[self._player]
+        return f'{board} {COLORS[self.player]} {self.variant}'
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
     cpdef get_current_team(self):
         if self.variant == "MRY":
-            if self._player in [5,0,1]:
+            if self.player in [5,0,1]:
                 return "MRY"
             else:
                 return "GCB"
         elif self.variant == "MR":
-            if self._player in [5,0]:
+            if self.player in [5,0]:
                 return "MR"
-            elif self._player in [1,2]:
+            elif self.player in [1,2]:
                 return "YG"
             else:
                 return "CB"
         else:
-            return self.player
+            return COLORS[self.player]
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
     cpdef get_legal_moves(self):
-        cdef char i, x, n = 0
+        cdef char i, j, c, n = 0
         cdef char[19][2] moves
         for i in range(19):
-            x = TRANSFORMATIONS[self._player][self.board[i]]
-            if x != -1:
-                moves[n] = [i,x]
-                n += 1
-        return [(moves[i][0],moves[i][1]) for i in range(n)]
+            c = self.board[i]
+            for j in range(4):
+                if c == TRANSFORMATIONS[self.player][j][0]:
+                    moves[n] = [i, TRANSFORMATIONS[self.player][j][1]]
+                    n += 1
+                    break
+        return [(moves[i][0], moves[i][1]) for i in range(n)]
 
     @cython.boundscheck(False)
     @cython.wraparound(False)
@@ -91,7 +86,7 @@ cdef class HexachromixState:
         cdef char i = move[0], c = move[1]
         board = [self.board[i] for i in range(19)]
         board[i] = c
-        return HexachromixState(board, (self._player+1)%6, self.variant)
+        return HexachromixState(board, (self.player+1)%6, self.variant)
 
     cpdef bint is_terminal(self):
         return self.has_path() or len(self.get_legal_moves()) == 0
@@ -100,12 +95,12 @@ cdef class HexachromixState:
         return 1 if self.has_path() else 0
 
     cdef bint has_path(self):
-        return bfs((self._player-1)%6, self.board)
+        return bfs((self.player-1)%6, self.board)
 
     def get_result(self):
         if not self.is_terminal(): return None
         if self.get_reward() == 0: return 'DRAW'
-        return COLORS[(self._player-1)%6]
+        return COLORS[(self.player-1)%6]
 
     def __repr__(self): return self.hfen
 
@@ -150,7 +145,7 @@ cdef char[6][2][3] SIDES = [
     [[11,15,18], [0,3,7]],
 ]
 
-cdef char[6][3] OCCUPANTS = [[ord(c) for c in chars] for chars in ['mRy','rYg','yGc','gCb','cBm','bMr']]
+cdef char[6][3] OCCUPANTS = [[CHAR2INT[c] for c in chars] for chars in ['mRy','rYg','yGc','gCb','cBm','bMr']]
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -159,9 +154,9 @@ cdef bint bfs(char color_idx, char[19] board):
     cdef bint does_occupy
     cdef char[3] ends = SIDES[color_idx][1]
     cdef bint[19] visited = [False] * 19
+    cdef char[19] frontier
 
     # Initialize the frontier with the three starting indices.
-    cdef char[19] frontier
     (frontier[0],frontier[1],frontier[2]) = SIDES[color_idx][0]
     n = 3
 
